0f908c2eccf6649da53202e566182707a7124380
Кирилл Биндюков <@itstep.krg>
2022-10-03 20:47:24 +0600

    create main file

---
+++ main.py
@@ -0,0 +1,17 @@
+import time
+
+def timer(fn):
+    def check_time(*args, **kwargs):
+        start_time = time.time()
+        res = fn()
+        time.sleep(1)
+        print(start_time - time.time())
+    return check_time
+
+
+@timer
+def my_range(n=0):
+    for i in range(n):
+        print(i, end=" ")
+
+print(my_range(600000))
